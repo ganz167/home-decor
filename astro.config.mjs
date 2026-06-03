333744091ecaf68fc91d76ce1463d7c7e9aba388
ganz167 <ganzodecor@gmail.com>
2026-06-03 08:43:04 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://ganzodecor.netlify.app',
-  integrations: [
-    tailwind(),
-    //sitemap(),
-    mdx(),
-  ],
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
-    },
-  },
-  image: {
-    domains: ['images.unsplash.com', 'm.media-amazon.com'],
-  },
+      theme: 'github-light'
+    }
+  }
 });
